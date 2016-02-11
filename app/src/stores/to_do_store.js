9b4c8135d0b4f9b611986d2adf03bcf078923df0
Ephraim Pei <ephraimpei@gmail.com>
2016-02-10 22:25:28 -0500

    todo store added

---
+++ app/src/stores/to_do_store.js
@@ -0,0 +1,64 @@
+import AppDispatcher from '../dispatcher/dispatcher.js';
+import ToDoConstants from "../constants/user_constants.js";
+import EventEmitter from 'eventemitter3';
+
+const CHANGE_EVENT = "change";
+
+class ToDoStore extends EventEmitter {
+  constructor () {
+    super();
+    this.toDos = [];
+  }
+
+  addChangeListener (callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener (callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  get () {
+    return this.toDos.slice();
+  }
+
+  add (toDo) {
+    const findToDo = this.toDos.find((el) => el === toDo);
+
+    if (typeof findUser === 'undefined') { this.toDos.push(toDo); }
+
+    this.emit(CHANGE_EVENT);
+  }
+
+  remove (toDo) {
+    const toDoIdx = this.toDos.indexOf(toDo);
+
+    if (toDoIdx !== -1) { this.toDos.splice(toDoIdx, 1);}
+
+    this.emit(CHANGE_EVENT);
+  }
+
+  set (toDos) {
+    this.toDos = toDos;
+
+    this.emit(CHANGE_EVENT);
+  }
+}
+
+const toDoStore = new ToDoStore();
+
+AppDispatcher.register(function (payload) {
+  switch (payload.actionType) {
+    case ToDoConstants.RECEIVE_TO_DOS:
+      toDoStore.set(payload.toDos);
+      break;
+    case ToDoConstants.RECEIVE_TO_DO:
+      toDoStore.add(payload.toDo);
+      break;
+    case ToDoConstants.DELETE_TO_DO:
+      toDoStore.remove(payload.toDo);
+      break;
+  }
+});
+
+export default toDoStore;
