3591f6b63ea5a89ca7de332bf5ce92e1b048da68
Ephraim Pei <ephraimpei@gmail.com>
2016-02-10 21:51:26 -0500

    fetch todos action complete

--- app/api/controllers/to_dos_controller.py
+++ app/api/controllers/to_dos_controller.py
@@ -4,31 +4,31 @@ from app.api.models import User, ToDo, ToDoForm
 import pdb
 
 @app.route("/user/<username>/todo", methods=["GET", "POST"])
-def handle_todo_list_request(username):
+def handle_to_do_list_request(username):
     if request.method == "GET":
-        return __fetch_todo_items(username)
+        return __fetch_to_do_items(username)
     elif request.method == "POST":
-        return __create_todo_item(username)
+        return __create_to_do_item(username)
 
-def __fetch_todo_items(username):
+def __fetch_to_do_items(username):
     user = User.find_by_username(username)
 
     if user:
-        todos = User.objects.get(username=user.username).todos
-        return jsonify(todos=todos)
+        to_dos = User.objects.get(username=user.username).to_dos
+        return jsonify(to_dos=to_dos)
     else:
         return jsonify(error="Could not find user."), 400
 
-def __create_todo_item(username):
+def __create_to_do_item(username):
     user = User.find_by_username(username)
 
     if user:
         form = ToDoForm(request.form)
 
         if form.validate():
-            new_todo = ToDo(title=form.title.data, description=form.description.data)
-            if user.update(add_to_set__to_dos=new_todo):
-                return jsonify(todo = new_todo,
+            new_to_do = ToDo(title=form.title.data, description=form.description.data)
+            if user.update(add_to_set__to_dos=new_to_do):
+                return jsonify(to_do = new_to_do,
                     message = "To do created successfully! Add some Pomodoros to it!")
             else:
                 return jsonify(error="Could not create to do item."), 401
