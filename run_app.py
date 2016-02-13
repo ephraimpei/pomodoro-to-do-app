ab5fcd3429165361cf7b3f82e176a05b87f1a09b
Ephraim Pei <ephraimpei@gmail.com>
2016-02-13 15:07:06 -0500

    added mongodb prod connection settings

--- run_app.py
+++ run_app.py
@@ -1,5 +1,9 @@
 import os
 from app import app
+from MongoEngine import connect
+
+connect('pomodoro-to-do-app',
+ host='mongodb://heroku_1d9g0hkc:h1aifmg4mssq7chsmki456jfk1@ds061395.mongolab.com:61395/heroku_1d9g0hkc')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
