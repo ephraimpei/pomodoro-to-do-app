c7b274aa8d520e888d0fa5c91fdb21b17412f27c
Ephraim Pei <ephraimpei@gmail.com>
2016-02-14 21:47:53 -0500

    modified app to load db config from ENV

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+pomodoro_db_name = os.environ['POMODORO_DB_NAME']
+mongolab_uri = os.environ['MONGOLAB_URI']
+
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+# MongoLab db connection settings
+app.config["MONGODB_SETTINGS"] = {
+    "DB": pomodoro_db_name,
+    "host": mongolab_uri
+}
+
+app.config["SECRET_KEY"] = "psst_im_a_secret"
+
+db = MongoEngine(app)
+
+from app.api.controllers import *
